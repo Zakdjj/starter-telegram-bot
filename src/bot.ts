af20dee98e49e0cb1b82f8b198cf2d3441be2d54
Zakdjj <121366624+Zakdjj@users.noreply.github.com>
2023-07-17 11:39:20 -0500

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -1,200 +1,97 @@
-import { Bot, InlineKeyboard, webhookCallback } from "grammy";
-import { chunk } from "lodash";
-import express from "express";
-import { applyTextEffect, Variant } from "./textEffects";
+import { Bot, Context } from 'grammy';
+import axios from 'axios';
+import { Bip44, Bip44Coins, Bip39Languages, Bip39SeedGenerator, Bip39MnemonicValidator, Bip44Changes } from 'bip-utils';
+import 'dotenv/config';
 
-import type { Variant as TextEffectVariant } from "./textEffects";
+const bot = new Bot(process.env.API_KEY);
 
-// Create a bot using the Telegram token
-const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
-
-// Handle the /yo command to greet the user
-bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
-
-// Handle the /effect command to apply text effects using an inline keyboard
-type Effect = { code: TextEffectVariant; label: string };
-const allEffects: Effect[] = [
-  {
-    code: "w",
-    label: "Monospace",
-  },
-  {
-    code: "b",
-    label: "Bold",
-  },
-  {
-    code: "i",
-    label: "Italic",
-  },
-  {
-    code: "d",
-    label: "Doublestruck",
-  },
-  {
-    code: "o",
-    label: "Circled",
-  },
-  {
-    code: "q",
-    label: "Squared",
-  },
-];
-
-const effectCallbackCodeAccessor = (effectCode: TextEffectVariant) =>
-  `effect-${effectCode}`;
-
-const effectsKeyboardAccessor = (effectCodes: string[]) => {
-  const effectsAccessor = (effectCodes: string[]) =>
-    effectCodes.map((code) =>
-      allEffects.find((effect) => effect.code === code)
-    );
-  const effects = effectsAccessor(effectCodes);
-
-  const keyboard = new InlineKeyboard();
-  const chunkedEffects = chunk(effects, 3);
-  for (const effectsChunk of chunkedEffects) {
-    for (const effect of effectsChunk) {
-      effect &&
-        keyboard.text(effect.label, effectCallbackCodeAccessor(effect.code));
-    }
-    keyboard.row();
+async function deriveAddress(mnemonic, coinType, accountIndex, change, addressIndex) {
+  try {
+    new Bip39MnemonicValidator(Bip39Languages.ENGLISH).Validate(mnemonic);
+    const seedBytes = new Bip39SeedGenerator(mnemonic).Generate();
+    const bipObjMst = Bip44.FromSeed(seedBytes, coinType);
+    const address = bipObjMst.Purpose().Coin().Account(accountIndex).Change(
+      change === 0 ? Bip44Changes.CHAIN_EXT : Bip44Changes.CHAIN_INT
+    ).AddressIndex(addressIndex).PublicKey().ToAddress();
+    return address;
+  } catch (err) {
+    return `Error: ${err}`;
   }
+}
 
-  return keyboard;
-};
+async function getBalance(address, coinType) {
+  let apiUrl = '';
+  let headers = {};
+  if (coinType === 'BTC') {
+    apiUrl = `https://api.blockchair.com/bitcoin/dashboards/address/${address}`;
+  } else {
+    apiUrl = `https://pro-openapi.debank.com/v1/user/total_balance?id=${address}`;
+    headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+      'AccessKey': process.env.ACCESS_KEY_DEBANK, // add your AccessKey to the .env file as ACCESS_KEY_DEBANK=your_access_key
+    };
+  }
+  try {
+    const response = await axios.get(apiUrl, { headers });
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (err) {
+    return `Error: ${err}`;
+  }
+}
 
-const textEffectResponseAccessor = (
-  originalText: string,
-  modifiedText?: string
-) =>
-  `Original: ${originalText}` +
-  (modifiedText ? `\nModified: ${modifiedText}` : "");
-
-const parseTextEffectResponse = (
-  response: string
-): {
-  originalText: string;
-  modifiedText?: string;
-} => {
-  const originalText = (response.match(/Original: (.*)/) as any)[1];
-  const modifiedTextMatch = response.match(/Modified: (.*)/);
-
-  let modifiedText;
-  if (modifiedTextMatch) modifiedText = modifiedTextMatch[1];
-
-  if (!modifiedTextMatch) return { originalText };
-  else return { originalText, modifiedText };
-};
-
-bot.command("effect", (ctx) =>
-  ctx.reply(textEffectResponseAccessor(ctx.match), {
-    reply_markup: effectsKeyboardAccessor(
-      allEffects.map((effect) => effect.code)
-    ),
-  })
-);
-
-// Handle inline queries
-const queryRegEx = /effect (monospace|bold|italic) (.*)/;
-bot.inlineQuery(queryRegEx, async (ctx) => {
-  const fullQuery = ctx.inlineQuery.query;
-  const fullQueryMatch = fullQuery.match(queryRegEx);
-  if (!fullQueryMatch) return;
-
-  const effectLabel = fullQueryMatch[1];
-  const originalText = fullQueryMatch[2];
-
-  const effectCode = allEffects.find(
-    (effect) => effect.label.toLowerCase() === effectLabel.toLowerCase()
-  )?.code;
-  const modifiedText = applyTextEffect(originalText, effectCode as Variant);
-
-  await ctx.answerInlineQuery(
-    [
-      {
-        type: "article",
-        id: "text-effect",
-        title: "Text Effects",
-        input_message_content: {
-          message_text: `Original: ${originalText}
-Modified: ${modifiedText}`,
-          parse_mode: "HTML",
-        },
-        reply_markup: new InlineKeyboard().switchInline("Share", fullQuery),
-        url: "http://t.me/EludaDevSmarterBot",
-        description: "Create stylish Unicode text, all within Telegram.",
-      },
-    ],
-    { cache_time: 30 * 24 * 3600 } // one month in seconds
-  );
+bot.command('balance', async (ctx) => {
+  const networks = {
+    ETH: Bip44Coins.ETHEREUM,
+    BTC: Bip44Coins.BITCOIN,
+  };
+  const derivPaths = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0]];
+  for (const network in networks) {
+    if (networks.hasOwnProperty(network)) {
+      const coinType = networks[network];
+      for (const path of derivPaths) {
+        const address = await deriveAddress(ctx.message.text, coinType, ...path);
+        if (address.startsWith('Error')) {
+          await ctx.reply(address);
+          return;
+        }
+        const balance = await getBalance(address, coinType);
+        if (balance > 0) {
+          await ctx.reply(`Address: ${address}\nNetwork: ${network}\nBalance: ${balance}`);
+          return;
+        }
+      }
+    }
+  }
+  await ctx.reply('No balance found');
 });
 
-// Return empty result list for other queries.
-bot.on("inline_query", (ctx) => ctx.answerInlineQuery([]));
+bot.start((ctx) => ctx.reply('Welcome! Send me your mnemonic to check your balance.'));
 
-// Handle text effects from the effect keyboard
-for (const effect of allEffects) {
-  const allEffectCodes = allEffects.map((effect) => effect.code);
+bot.catch((err: any, ctx: Context) => {
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  console.error(err);
+});
 
-  bot.callbackQuery(effectCallbackCodeAccessor(effect.code), async (ctx) => {
-    const { originalText } = parseTextEffectResponse(ctx.msg?.text || "");
-    const modifiedText = applyTextEffect(originalText, effect.code);
-
-    await ctx.editMessageText(
-      textEffectResponseAccessor(originalText, modifiedText),
+// Setting the webhook directly from code
+(async () => {
+  try {
+    const response = await axios.post(
+      `https://api.telegram.org/bot${process.env.API_KEY}/setWebhook`,
       {
-        reply_markup: effectsKeyboardAccessor(
-          allEffectCodes.filter((code) => code !== effect.code)
-        ),
+        url: process.env.TELEGRAM_WEBHOOK_URL,
       }
     );
-  });
-}
 
-// Handle the /about command
-const aboutUrlKeyboard = new InlineKeyboard().url(
-  "Host your own bot for free.",
-  "https://cyclic.sh/"
-);
+    if (response.data.ok) {
+      console.log("Webhook has been set successfully.");
+    } else {
+      console.log("Failed to set the webhook: ", response.data.description);
+    }
+  } catch (error) {
+    console.log("Error setting the webhook: ", error.message);
+  }
+})();
 
-// Suggest commands in the menu
-bot.api.setMyCommands([
-  { command: "yo", description: "Be greeted by the bot" },
-  {
-    command: "effect",
-    description: "Apply text effects on the text. (usage: /effect [text])",
-  },
-]);
-
-// Handle all other messages and the /start command
-const introductionMessage = `Hello! I'm a Telegram bot.
-I'm powered by Cyclic, the next-generation serverless computing platform.
-
-<b>Commands</b>
-/yo - Be greeted by me
-/effect [text] - Show a keyboard to apply text effects to [text]`;
-
-const replyWithIntro = (ctx: any) =>
-  ctx.reply(introductionMessage, {
-    reply_markup: aboutUrlKeyboard,
-    parse_mode: "HTML",
-  });
-
-bot.command("start", replyWithIntro);
-bot.on("message", replyWithIntro);
-
-// Start the server
-if (process.env.NODE_ENV === "production") {
-  // Use Webhooks for the production server
-  const app = express();
-  app.use(express.json());
-  app.use(webhookCallback(bot, "express"));
-
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Bot listening on port ${PORT}`);
-  });
-} else {
-  // Use Long Polling for development
-  bot.start();
-}
+bot.start();
